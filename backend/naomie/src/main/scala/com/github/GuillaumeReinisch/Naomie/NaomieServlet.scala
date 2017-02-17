bcf4ea36670fd537e56d72dfb1804182b1b04ff8
Guillaume Reinisch <reinisch.gui@gmail.com>
2017-02-17 13:44:00 +0100

    add graphics mockup and http getGraphics route

--- backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieServlet.scala
+++ backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieServlet.scala
@@ -1,8 +1,31 @@
 package com.github.GuillaumeReinisch.Naomie
 
-class NaomieServlet extends NaomieStack {
+import org.slf4j.LoggerFactory
+
+// JSON-related libraries
+import org.json4s.{DefaultFormats, Formats}
+
+// JSON handling support from Scalatra
+import org.scalatra.json._
+
+case class Graphic( id: String, name: String , axis: List[String]){}
+
+object GraphicsData {
+
+  /**
+    * Some fake data so we can simulate retrievals.
+    */
+  var all = List(
+    Graphic("0", "temperature", List("oslo","paris")),
+    Graphic("1", "pressure",List("oslo","paris")),
+    Graphic("2", "wind",List("oslo","paris")))
+}
+
+
+class NaomieServlet extends NaomieStack with JacksonJsonSupport {
 
   get("/") {
+    contentType = formats("html")
     <html>
       <body>
         <h1>Hello, world!</h1>
@@ -11,4 +34,7 @@ class NaomieServlet extends NaomieStack {
     </html>
   }
 
+  get("/graphics") {
+    GraphicsData.all;
+  }
 }
