a73dd682fa34dddd496ada8075faf0b2b4100615
Guillaume Reinisch <reinisch.gui@gmail.com>
2017-03-05 16:15:39 +0100

    [backend - swagger]

--- backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieStack.scala
+++ backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieStack.scala
@@ -4,7 +4,7 @@ import org.scalatra._
 import scalate.ScalateSupport
 import org.fusesource.scalate.{ TemplateEngine, Binding }
 import org.fusesource.scalate.layout.DefaultLayoutStrategy
-import javax.servlet.http.HttpServletRequest
+import org.scalatra.scalate.ScalateSupport
 import collection.mutable
 
 trait NaomieStack extends ScalatraServlet with ScalateSupport {
--- backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieSwagger.scala
+++ backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/NaomieSwagger.scala
@@ -4,8 +4,17 @@ package com.github.GuillaumeReinisch.Naomie
 import org.scalatra.ScalatraServlet
 import org.scalatra.swagger.{ApiInfo, JacksonSwaggerBase, Swagger}
 
-class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with JacksonSwaggerBase {
+class ResourcesApp(implicit val swagger: Swagger) extends ScalatraServlet with JacksonSwaggerBase
 
+
+class NaomieSwagger extends Swagger(apiInfo = ApiSwagger.apiInfo, apiVersion = "1.0", swaggerVersion = "1.2")
+
+object ApiSwagger {
+  val apiInfo = ApiInfo(
+    """Swagger Petstore""",
+    """This is a sample server Petstore server.  You can find out more about Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).  For this sample, you can use the api key `special-key` to test the authorization filters.""",
+    """""",
+    """apiteam@swagger.io""",
+    """Apache 2.0""",
+    """http://www.apache.org/licenses/LICENSE-2.0.html""")
 }
-
-class NaomieSwagger extends Swagger("1.0", "1",ApiInfo("","","","","",""))
