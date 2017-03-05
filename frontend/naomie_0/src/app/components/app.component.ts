ec59482d3d86332004b918790f7d40c9f40c4ff1
Guillaume Reinisch <reinisch.gui@gmail.com>
2017-03-05 16:56:17 +0100

    [Front  end - rename route]

--- frontend/naomie_0/src/app/components/app.component.ts
+++ frontend/naomie_0/src/app/components/app.component.ts
@@ -3,10 +3,10 @@ import { Component , ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{name}}</h1>  
+  template: `<h1>{{name}}</h1>
              <a routerLink="/dashboard">dashboard</a>
              <a routerLink="/graphics">graphics</a>
-             <a routerLink="/data">data</a>
+             <a routerLink="/scenarios">data</a>
              <router-outlet></router-outlet>
   `,
 })
