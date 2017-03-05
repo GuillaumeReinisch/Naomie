0b2b742ed8bb2945df3aa57839aaf7771e3eed2b
Guillaume Reinisch <reinisch.gui@gmail.com>
2017-03-05 16:11:40 +0100

    [backend - dataset] : add construction of dataset from entity

--- backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/models/Scenario.scala
+++ backend/naomie/src/main/scala/com/github/GuillaumeReinisch/Naomie/models/Scenario.scala
@@ -1,7 +1,26 @@
 package com.github.GuillaumeReinisch.Naomie.models
 import com.google.cloud.datastore._
+import org.slf4j.LoggerFactory
 
 case class Dataset( val name : String, val scenarioUid: String,  val vheaders: Vector[String], val columns: Map[String,Vector[Double]] , val metadata : Map[String,String] = Map[String,String]() ){}
+
+object Dataset {
+  val logger    =  LoggerFactory.getLogger(getClass)
+  def apply( entity : Entity) : Dataset = {
+
+    val headers = entity.getString("hheaders").split(" ").toList.drop(1);
+    logger.info("dataset's headers:" + headers.toString);
+
+    val columns = for { hHeader <- headers }
+      yield (hHeader, entity.getString(hHeader).split(" ").toList.drop(1).map( _.toDouble ).toVector)
+
+    Dataset(entity.getKey().getName(),
+            entity.getString("parent"),
+            entity.getString("vheaders").split(" ").toVector,
+            columns.toMap )
+  }
+}
+
 
 case class Scenario( val uid: String, val name: String , val metadata : Map[String,String]) {
 }
